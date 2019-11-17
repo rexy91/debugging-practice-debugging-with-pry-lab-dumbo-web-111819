df66f2f642fcc90b65230baad8527c27ae4d2c18
Yu Hao Ye <rxye1991@gmail.com>
2019-11-17 00:12:56 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,12 @@
 def snake_it_up(string)
-  if string[0] == "s"
-  10 * "s" + string
-  else
-  string
-  end
+    result = []
+    if string[0] == 's'
+        i = 0
+        while i < 10 do 
+            result << 's'
+        i += 1 
+    end
+    end
+    result << string 
+    result.join("")
 end
