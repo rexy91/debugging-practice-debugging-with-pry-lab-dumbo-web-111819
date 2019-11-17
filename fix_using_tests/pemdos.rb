17b7774bd74de71f9c4e4c19dc13b1fa5ff2dbdf
Yu Hao Ye <rxye1991@gmail.com>
2019-11-17 00:12:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,21 @@
 def snake_it_up(string)
   if string[0] == "s"
+  
   10 * "s" + string
   else
   string
   end
 end
+
+
+def snake_it_up(string)
+  result = []
+  if string[0] == 's'
+    i = 0
+    while i < 10 do 
+      result << 's'
+    i += 1
+  end
+end
+  
+      
